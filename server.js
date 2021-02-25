9b87443c74855d856ab1bfb7a1d612e8433b4164
Samirul Alam <samirulalam372@gmail.com>
2021-02-25 17:08:46 +0600

    backend completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import Pusher from "pusher";
 import dotenv from 'dotenv';
 import Cors from "cors";
 dotenv.config();
+import conversation from "./dbModel.js";
 
 //app config
 const app = express();
@@ -36,6 +37,73 @@ mongoose.connection.once("open",()=>{
 
 //api routes
 app.get("/",(req, res) => {res.status(200).send("hello")})
+
+app.post("/new/channel",(req, res) => {
+    const dbData=req.body;
+
+    conversation.create(dbData,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get("/get/channelList",(req, res) => {
+    conversation.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            let channels=[];
+            data.map((channelData)=>{
+                const channelInfo={
+                    id:channelData._id,
+                    name:channelData.channelName
+                }
+                channels.push(channelInfo)
+            })
+            res.status(200).send(channels);
+        }
+    })
+})
+
+app.post('/new/message',(req, res)=>{
+    const newMessage=req.body;
+
+    conversation.update(
+        {_id: req.query.id},
+        {$push:{conversation:req.body}},
+        (err,data)=>{
+            if(err){
+                res.status(500).send(err)
+            }else{
+                res.status(200).send(data)
+            }
+        }
+    )
+})
+
+app.get("/get/data",(req, res)=>{
+    conversation.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get("/get/conversation",(req, res)=>{
+    const id=req.query.id;
+    conversation.find({_id:id},(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
 
 //listener
 app.listen(port,()=>console.log(`listening to port ${port}`));
